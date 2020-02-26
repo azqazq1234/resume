69300b4e6342282c3a6d148ba6d5e8017ce11615
harvey <zhoulihua01@chinatopcredit.com>
2020-02-26 09:59:18 +0800

    init test

---
+++ build/precache-manifest.2499d925928ad3ed262fed9fc79d7fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575b96b0e10f51541922a72b2aa710b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7cf11ae6fc0c6d07252e",
+    "url": "./static/css/2.6d855ee2.chunk.css"
+  },
+  {
+    "revision": "c4f608e6aee4f71d67fc",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7cf11ae6fc0c6d07252e",
+    "url": "./static/js/2.7fec68c9.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "./static/js/2.7fec68c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f608e6aee4f71d67fc",
+    "url": "./static/js/main.b634820f.chunk.js"
+  },
+  {
+    "revision": "44a06ee2722ae10c6778",
+    "url": "./static/js/runtime-main.97a7e1ab.js"
+  }
+]);
